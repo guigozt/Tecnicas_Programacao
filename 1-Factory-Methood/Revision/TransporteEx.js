a9f9bdaf642bb3b9552b944518ac3d5ccfadd738
Guilherme <guilhermegomeszt@gmail.com>
2026-04-01 08:34:08 -0300

    revisão-sobre-factory-method

---
+++ 1-Factory-Methood/Revision/TransporteEx.js
@@ -0,0 +1,45 @@
+//Produto (Interface)
+class Transporte {
+    entregar() {
+        throw new Error("Método entregar deve ser implementado")
+    }
+}
+
+//Produtos concretos
+class Carro extends Transporte {
+    entregar() {
+        console.log("Entrega de CARRO 🚗")
+    }
+}
+
+class Moto extends Transporte {
+    entregar() {
+        console.log("Entrega de MOTO 🛵")
+    }
+}
+
+//Fabrica
+class FabricaTransporte {
+    criarTransporte(veiculo) {
+        if (veiculo === "carro") {
+            return new Carro()
+        }
+        else if (veiculo === "moto") {
+            return new Moto()
+        }
+        else {
+            throw new Error("Veiculo inválido...")
+        }
+    }
+}
+
+// Cliente
+const fabrica = new FabricaTransporte()
+
+const transporte1 = fabrica.criarTransporte("carro")
+transporte1.entregar()
+
+console.log("\n-------------------\n")
+
+const transporte2 = fabrica.criarTransporte("moto")
+transporte1.entregar()
